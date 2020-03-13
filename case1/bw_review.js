b90fa0d29dd534e8673f57b3ceb94781421fec47
Yelaina <ybuford201@west-mec.org>
2020-03-12 17:57:56 -0700

    finished case 1

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -42,26 +42,51 @@ function init(){
       stars[i].style.cursor = "pointer";
       stars[i].addEventListener("mouseenter", lightStars);
    }
-   document.addEventListener("keyup", Count);
+   document.getElementById("commentField").addEventListener("keyup", updateCount);
 }
 
 function lightStars(e){
    var starNumber = e.target.alt;
    var stars = document.querySelectorAll("span#stars img");
 
-   for(var i = 0; i < starNumber.length; i++){
-      starNumber[i]
+   for(var i = 0; i < stars.length; i++){
+      stars[i].src = "bw_star2.png";
    }
+   for(var i = starNumber; i < 5 ;i++){
+      stars[i].src = "bw_star.png";
+   }
+
+   document.getElementById("rating").value = starNumber + " stars"
+   e.target.addEventListener("mouseleave", turnOffStars)
+
+   document.addEventListener("click", 
+   function(){
+      e.target.removeEventListener("mouseleave", turnOffStars);
+      }
+   );
 }
 
-function turnOffStars(e){
+function turnOffStars(){
    var stars = document.querySelectorAll("span#stars img");
    for(var i = 0; i < stars.length; i++){
-      
+      stars[i].src = "bw_star.png";
    }
+   document.getElementById("rating").value = "";
 }
 
 function updateCount(){
+   var commentText = document.getElementById("comment").value;
+   var charCount = countCharacters(commentText);
+   var wordCountBox = document.getElementById("wordCount");
+
+   wordCountBox.value = charCount + "/1000"
+   if(charCount > 1000){
+      wordCountBox.style.backgroundColor = "red";
+      wordCountBox.style.color = "white";
+   }else{
+      wordCountBox.style.backgroundColor = "white";
+      wordCountBox.style.color = "black";
+   }
 
 }
 
